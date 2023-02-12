3a4072db8096590dbc1ec47a109200c4c8be13db
atharva01903 <atharvayogesh.d19@iiits.in>
2023-02-13 02:20:39 +0530

    Try accessing second page from the first

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Form from "../components/Form";
 
 const FirstPage = () => {
+    const navigate = useNavigate();
+
+    const clearStorageReload = () => {
+      localStorage.removeItem("userDataKey");
+      window.location.reload();
+    };
+
+    const visitSecondPage = () => {
+        navigate("/second-page")
+    }
     return(
         <div>
             <h1>Enter user information</h1>
             <Form/>
+            <br />
+            <button onClick={clearStorageReload}>
+                Clear local storage and reload
+            </button>
+            <br />
+            <br />
+            <button onClick={visitSecondPage}>
+                Visit Second Page
+            </button>
         </div>
     )
 }
